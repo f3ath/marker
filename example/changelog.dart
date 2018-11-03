43675c0a7efe7a7c39edea00d14cb83015f4339e
Alexey Karapetov <karapetov@gmail.com>
2018-11-02 22:44:18 -0700

    Format

--- example/changelog.dart
+++ example/changelog.dart
@@ -6,8 +6,10 @@ import 'package:marker/marker.dart';
 /// Renders CHANGELOG.md with all links inline
 void main() {
   final file = File('CHANGELOG.md');
+
   /// Contains parsed tree
   final nodes = Document().parseLines(file.readAsLinesSync());
+
   /// Contains markdown text
   final markdown = render(nodes);
   print(markdown);
